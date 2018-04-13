5cc4d1964d6071117d1a109512096c90e6a197d0
xxxx <xxxxx@qq.com>
2018-04-13 15:46:52 +0800

    img

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,31 +18,36 @@ export class HomePage {
 
     ionViewDidLoad() {
         let loading = this.loadingCtrl.create({
-            content: 'Please wait...'
+            content: '正在加载...'
         });
 
         loading.present();
         // 网络请求
+        this.listData = [{name: '北京靠谱科技有限公司上市计划', email: '管理员  04-13 14:51',img:'card-amsterdam.png',icon:'Browser-Icon.png',slide:'slide1.jpeg'},
+            {name: '100家老字号企业集体亮相', email: '管理员  04-13 14:51',img:'card-madison.png',icon:'Clock.png',slide:'slide2.jpeg'},
+            {name: '大数据解决方案实施', email: '管理员  04-13 14:51',img:'card-saopaolo.png',icon:'user.png',slide:'slide3.jpeg'},
+            {name: '抗战胜利70周年纪念日', email: '管理员  04-13 14:51',img:'card-sf.png',icon:'Key.png',slide:'slide4.jpeg'},
+            {name: '北京靠谱科技有限公司', email: '管理员  04-13 14:51',img:'card-amsterdam.png',icon:'Gear.png',slide:'slide5.jpeg'},
+            {name: '100家老字号企业集体亮相', email: '管理员  04-13 14:51',img:'card-madison.png',icon:'Clock.png',slide:'slide4.jpeg'},
+            {name: '大数据解决方案实施', email: '管理员  04-13 14:51',img:'card-saopaolo.png',icon:'Cloud.png',slide:'slide2.jpeg'},
+            {name: '抗战胜利70周年纪念日', email: '管理员  04-13 14:51',img:'card-sf.png',icon:'Code-Window.png',slide:'slide2.jpeg'},
+            {name: '北京靠谱科技有限公司', email: '管理员  04-13 14:51',img:'card-amsterdam.png',icon:'Contract.png',slide:'slide4.jpeg'},
+            {name: '100家老字号企业集体亮相', email: '管理员  04-13 14:51',img:'card-madison.png',icon:'Desk.png',slide:'slide2.jpeg'},
+            {name: '大数据解决方案实施', email: '管理员  04-13 14:51',img:'card-saopaolo.png',icon:'Desktop.png',slide:'slide4.jpeg'},
+            {name: '抗战胜利70周年纪念日', email: '管理员  04-13 14:51',img:'card-sf.png',icon:'Documents.png',slide:'slide2.jpeg'},
+            {name: '北京靠谱科技有限公司', email: '管理员  04-13 14:51',img:'card-amsterdam.png',icon:'Drawing-Tablet.png',slide:'slide4.jpeg'},
+            {name: '100家老字号企业集体亮相', email: '管理员  04-13 14:51',img:'card-madison.png',icon:'Envelope.png',slide:'slide2.jpeg'},
+            {name: '大数据解决方案实施', email: '管理员  04-13 14:51',img:'card-saopaolo.png',icon:'Eye.png',slide:'slide4.jpeg'},
+            {name: '抗战胜利70周年纪念日', email: '管理员  04-13 14:51',img:'card-sf.png',icon:'Gear.png',slide:'slide2.jpeg'}];
+
         this.http.request('http://jsonplaceholder.typicode.com/users')
             .subscribe((res: Response) => {
 
                 //this.listData = res.json();
-                this.listData = [{name: '北京纽伦科技有限公司', email: '阅读人数:141',img:'card-amsterdam.png',icon:'Battery.png'},
-                    {name: '100家老字号企业集体亮相', email: '阅读人数:3021',img:'card-madison.png',icon:'Briefcase.png'},
-                    {name: '大数据解决方案实施', email: '阅读人数:41',img:'card-saopaolo.png',icon:'Browser-Icon.png'},
-                    {name: '抗战胜利70周年纪念日', email: '阅读人数:3',img:'card-sf.png',icon:'Browser-Testing.png'},
-                    {name: '北京纽伦科技有限公司', email: '阅读人数:141',img:'card-amsterdam.png',icon:'Camera.png'},
-                    {name: '100家老字号企业集体亮相', email: '阅读人数:3021',img:'card-madison.png',icon:'Clock.png'},
-                    {name: '大数据解决方案实施', email: '阅读人数:41',img:'card-saopaolo.png',icon:'Cloud.png'},
-                    {name: '抗战胜利70周年纪念日', email: '阅读人数:3',img:'card-sf.png',icon:'Code-Window.png'},
-                    {name: '北京纽伦科技有限公司', email: '阅读人数:141',img:'card-amsterdam.png',icon:'Contract.png'},
-                    {name: '100家老字号企业集体亮相', email: '阅读人数:3021',img:'card-madison.png',icon:'Desk.png'},
-                    {name: '大数据解决方案实施', email: '阅读人数:41',img:'card-saopaolo.png',icon:'Desktop.png'},
-                    {name: '抗战胜利70周年纪念日', email: '阅读人数:3',img:'card-sf.png',icon:'Documents.png'},
-                    {name: '北京纽伦科技有限公司', email: '阅读人数:141',img:'card-amsterdam.png',icon:'Drawing-Tablet.png'},
-                    {name: '100家老字号企业集体亮相', email: '阅读人数:3021',img:'card-madison.png',icon:'Envelope.png'},
-                    {name: '大数据解决方案实施', email: '阅读人数:41',img:'card-saopaolo.png',icon:'Eye.png'},
-                    {name: '抗战胜利70周年纪念日', email: '阅读人数:3',img:'card-sf.png',icon:'Gear.png'}];
+
+                loading.dismiss();
+            },error=>{
+
                 loading.dismiss();
             });
     }
